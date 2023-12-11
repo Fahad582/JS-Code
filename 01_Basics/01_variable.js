9d385cec2465be6208c7837f10962f6b78f913c0
Fahad582 <107261204+Fahad582@users.noreply.github.com>
2023-12-11 20:35:17 +0000

    test

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 123
+let accountemail = "fahadt582@gmail.com"
+var accountpassword = "1234567890"
+accountcity ="Lahore"
+
+//accountId = 12 not allowed
+accountemail ="saadtariq05@gmail.com"
+accountpassword="121212"
+accountcity = "Multan"
+let accountstate;
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+
+console.log(accountId);
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all js code is new version
+
+//alert(3+3); we are using node this helpful for google browser
+console.log(3+3)
+console.log("Fahad")
